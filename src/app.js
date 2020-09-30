8bc66c0cbce5b2634bfafef4110a18f6d72066f8
Vitu Silva <marcelo.victor05@gmail.com>
2020-09-29 21:02:41 -0300

    altera comentario

--- src/app.js
+++ src/app.js
@@ -22,6 +22,6 @@ server.delete("/teste_delete", (req, res) => {
   return res.status(200).send({ name: "Tobinha POST" });
 });
 
-// alteração
+// CHUPADA DIMENSIONAL REVERSA
 
 server.listen(3000, () => console.log(`Serve on POST 3000`));
